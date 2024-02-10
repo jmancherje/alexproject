81551867816ca8133091fda4040c2017c99c2622
agolomb <adgolomb@gmail.com>
2024-02-09 18:40:16 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      Hello Alex
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
