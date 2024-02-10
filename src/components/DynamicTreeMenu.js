aa5731edd8267d1d3a3da6a0a72f323329b487fe
Justin Mancherje <jmancherje@nextdoor.com>
2024-02-10 11:36:47 -0800

    Render tree dynamically

--- src/components/DynamicTreeMenu.js
+++ src/components/DynamicTreeMenu.js
@@ -1,59 +1,60 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import { Col, Row } from "react-bootstrap";
 import "./dynamictreemenu.css";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import LastPageIcon from "@mui/icons-material/LastPage";
+import AlbumIcon from "@mui/icons-material/Album";
 import { TreeView } from "@mui/x-tree-view/TreeView";
 import { TreeItem } from "@mui/x-tree-view/TreeItem";
+
+const dynamicTree = {
+  label: "Root",
+  nodeId: "0",
+  children: [
+    {
+      label: "Parent A",
+      nodeId: "1",
+      children: [
+        {
+          label: "Child A1",
+          nodeId: "2",
+          children: [],
+        },
+        {
+          label: "Child Parent A2",
+          nodeId: "3",
+          children: [
+            {
+              label: "Child A21",
+              nodeId: "4",
+              children: [],
+            },
+            { label: "Child A22", nodeId: "5", children: [] },
+          ],
+        },
+      ],
+    },
+    {
+      label: "Parent B",
+      nodeId: "6",
+      children: [
+        { label: "Child B1", nodeId: "7", children: [] },
+        { label: "Child B2", nodeId: "8", children: [] },
+        {
+          label: "Child B3",
+          nodeId: "9",
+          children: [{ label: "Child Parent B31", nodeId: "10", children: [] }],
+        },
+      ],
+    },
+  ],
+};
+
 const DynamicTreeMenu = () => {
-  // const [clickedItemLabel, setClickedItemLabel] = useState("");
-  // const handleItemClick = (label) => {
-  //   setClickedItemLabel(label);
-  // };
-  const dynamicTree = {
-    label: "Root",
-    nodeId: "0",
-    children: [
-      {
-        label: "Parent A",
-        nodeId: "1",
-        children: [
-          {
-            label: "Child A1",
-            nodeId: "2",
-            children: [],
-          },
-          {
-            label: "Child Parent A2",
-            nodeId: "3",
-            children: [
-              {
-                label: "Child A21",
-                nodeId: "4",
-                children: [],
-              },
-              { label: "Child A22", nodeId: "5", children: [] },
-            ],
-          },
-        ],
-      },
-      {
-        label: "Parent B",
-        nodeId: "6",
-        children: [
-          { label: "Child B1", nodeId: "7", children: [] },
-          { label: "Child B2", nodeId: "8", children: [] },
-          {
-            label: "Child B3",
-            nodeId: "9",
-            children: [
-              { label: "Child Parent B31", nodeId: "10", children: [] },
-            ],
-          },
-        ],
-      },
-    ],
+  const [clickedItemLabel, setClickedItemLabel] = useState("");
+  const handleItemClick = (label) => {
+    setClickedItemLabel(label);
   };
 
   return (
@@ -69,62 +70,7 @@ const DynamicTreeMenu = () => {
               overflowY: "auto",
             }}
           >
-            <TreeItem nodeId="0" label="Root">
-              {dynamicTree.children.map((tree) => (
-                <TreeItem
-                  key={tree.nodeId}
-                  label={tree.label}
-                  endIcon={tree.children.length === 0 ? <LastPageIcon /> : null}
-                />
-              ))}
-            </TreeItem>
-
-            {/* <TreeItem nodeId="0" label="Root">
-              <TreeItem nodeId="1" label="Parent A">
-                <TreeItem
-                  nodeId="2"
-                  label="Child A1"
-                  endIcon={<LastPageIcon />}
-                  onClick={() => handleItemClick("Child A1")}
-                />
-                <TreeItem nodeId="3" label="Child Parent A2">
-                  <TreeItem
-                    nodeId="4"
-                    label="Child A21"
-                    endIcon={<LastPageIcon />}
-                    onClick={() => handleItemClick("Child A21")}
-                  />
-                  <TreeItem
-                    nodeId="5"
-                    label="Child A22"
-                    endIcon={<LastPageIcon />}
-                    onClick={() => handleItemClick("Child A22")}
-                  ></TreeItem>
-                </TreeItem>
-              </TreeItem>
-              <TreeItem nodeId="6" label="Parent B">
-                <TreeItem
-                  nodeId="7"
-                  label="Child B1"
-                  endIcon={<LastPageIcon />}
-                  onClick={() => handleItemClick("Child B1")}
-                />
-                <TreeItem
-                  nodeId="8"
-                  label="Child B2"
-                  endIcon={<LastPageIcon />}
-                  onClick={() => handleItemClick("Child B2")}
-                />
-                <TreeItem nodeId="9" label="Child B3">
-                  <TreeItem
-                    nodeId="10"
-                    label="Child Parent B31"
-                    endIcon={<LastPageIcon />}
-                    onClick={() => handleItemClick("Child B3")}
-                  />
-                </TreeItem>
-              </TreeItem>
-            </TreeItem> */}
+            <Tree tree={dynamicTree} onClick={handleItemClick} />
           </TreeView>
         </Col>
         <Col lg={6} className="bg-info">
@@ -140,12 +86,27 @@ const DynamicTreeMenu = () => {
         </Col>
       </Row>
       <Row>
-        {/* <Col lg={12} className="bg-danger">
+        <Col lg={12} className="bg-danger">
           {clickedItemLabel && <p>You are viewing: {clickedItemLabel}</p>}
-        </Col> */}
+        </Col>
       </Row>
     </Fragment>
   );
 };
+
+function Tree({ tree, onClick }) {
+  return (
+    <TreeItem
+      nodeId={tree.nodeId}
+      label={tree.label}
+      endIcon={tree.children.length === 0 ? <AlbumIcon /> : null}
+      onClick={() => onClick(tree.label)}
+    >
+      {tree.children.map((childTree) => (
+        <Tree key={tree.nodeId} tree={childTree} onClick={onClick} />
+      ))}
+    </TreeItem>
+  );
+}
 
 export default DynamicTreeMenu;
